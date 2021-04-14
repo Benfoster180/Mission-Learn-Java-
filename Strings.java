0bfdf1cb048e26167951e150e1b16a8168b5ae3d
Ben Foster <31930426+Benfoster180@users.noreply.github.com>
2021-04-14 13:00:54 +0100

    Just a bit about strings : o

---
+++ Strings.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    String first_name = "Frank";
+    String second_name = "Brad";
+    String fullName = first_name + " " + second_name;
+    System.out.println("Hello " + first_name);
+    System.out.println("or " + fullName);
+    int answer = 5;
+    System.out.println("Did you know 5 x 1 = " + answer);
+    
+  }
+}
