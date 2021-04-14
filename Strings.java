15b2be6714468c741acf00575d68223a5c17993c
Ben Foster <31930426+Benfoster180@users.noreply.github.com>
2021-04-14 13:16:46 +0100

    Just some random things I did with strings!

---
+++ Strings.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+      String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      String numbers = "0123456789";
+      //Prints length of a variable 
+      System.out.println(txt.length());
+      //Makes everything Upper case
+      System.out.println(txt.toUpperCase());
+      //Makes everything to Lower Case
+      System.out.println(txt.toLowerCase());
+      //Out puts psotion of certian term
+      System.out.println(numbers.indexOf("5"));
+      System.out.println("\n1\n2\n3\n4\n5\n");
+    }
+  }
